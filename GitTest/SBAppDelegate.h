0a8b9af784cb151f54b4b5c6c3730ea8b54165aa
Steve Brito <sbrito85@gmail.com>
2014-02-17 22:33:04 -0800

    Initial Commit

---
+++ GitTest/SBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SBAppDelegate.h
+//  GitTest
+//
+//  Created by Steve Brito on 2/17/14.
+//  Copyright (c) 2014 Steve Brito. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitTest/SBViewController.h
@@ -0,0 +1,13 @@
+//
+//  SBViewController.h
+//  GitTest
+//
+//  Created by Steve Brito on 2/17/14.
+//  Copyright (c) 2014 Steve Brito. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SBViewController : UIViewController
+
+@end
